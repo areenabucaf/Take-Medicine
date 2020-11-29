07a9e6ee890c6abc581a08d5eb452317e1eb9229
Areen aldda <74857785+areenabucaf@users.noreply.github.com>
2020-11-29 17:29:07 +0200

    the first page log in that check if the user is existing

---
+++ MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+    private Button log;
+    private Button sign;
+    private EditText email ;
+    private EditText pass ;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide(); //<< this
+        mAuth = FirebaseAuth.getInstance();
+
+        email = (EditText) findViewById(R.id.email);
+        pass = (EditText) findViewById(R.id.pass);
+
+        sign = (Button) findViewById(R.id.sign);
+        sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Uploading.class);
+                startActivity(i);
+            }
+        });
+
+        log = (Button) findViewById(R.id.log);
+        log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final Intent l = new Intent(MainActivity.this, Uploading.class);
+
+                if (email.getText().length()!= 0 && pass.getText().length()!= 0 ) {
+
+                    logInFunction(email.getText().toString() , pass.getText().toString());
+                }
+
+                else {
+
+                    Toast.makeText(MainActivity.this , "תכניס הפרטיים", Toast.LENGTH_LONG).show();
+                }
+            }
+
+
+        });
+
+
+    }
+
+    public void logInFunction(final String userName , String password ) {
+        mAuth.signInWithEmailAndPassword(userName , password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            Intent i = new Intent(MainActivity.this, Uploading.class);
+                            i.putExtra("userName", userName );
+                            startActivity(i);
+                            finish();
+                        } else
+                        {
+                            Toast.makeText(MainActivity.this, "not existing ", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
