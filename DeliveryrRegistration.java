b9656de40d57190b51fcea5559470bfcb693f488
Abeer-Tlalka <74859437+Abeer-Tlalka@users.noreply.github.com>
2021-01-03 21:31:47 +0200

    Deliveryr Registration

---
+++ DeliveryrRegistration.java
@@ -0,0 +1,14 @@
+package my.takeMedDesign.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeliveryrRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deliveryr_registration);
+    }
+}
