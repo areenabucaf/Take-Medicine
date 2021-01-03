b60ae21ac40301baae367fc1dd2caa7957ccc203
Abeer-Tlalka <74859437+Abeer-Tlalka@users.noreply.github.com>
2021-01-03 21:15:57 +0200

    The LogIn screen

---
+++ CustomerLogin.java
@@ -0,0 +1,13 @@
+package my.takeMedDesign.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomerLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_login);
+    }
+}
